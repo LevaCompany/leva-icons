9d7b47ccbeba6e0adc4b8b8e59d82fac5b96f995
Leva <levbulychou1234567890@outlook.com>
2022-11-16 11:35:40 +0300

    Update index.vue

--- index.vue
+++ index.vue
@@ -1,3 +1,3 @@
 <FrameWork src="framework.vuejs">
-<conectVault src="less/vue-js.less" type="text/less">
+<connectVault src="less/vue-js.less" type="text/less">
 <Leva-Icon type="leva-logo" icon="leva-company-cimpit-logo-svg-badge">
