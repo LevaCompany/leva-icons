60b2ed07d5a1cb3db93720bd326b3da8538aa3df
Leva <levbulychou1234567890@outlook.com>
2023-11-29 14:43:13 +0300

    Update debugging
    Now debugging have bugchecker

--- js/debug.js
+++ js/debug.js
@@ -1,10 +1,13 @@
 let iconsVar = 510;
-let length = 3;
+let length = 10;
 let elementIndex = 10000;
 const binCode =
 debugger{
     importScripts: true;
     textTrack: true;
+};
+const bugChecker{
+    let maxBugs=100;
 };
 
 for (let index = 0; index < array.length; index++) {
