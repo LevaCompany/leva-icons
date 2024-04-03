e89a84bf2186a8430098209853c668e211ff455c
Leva <levbulychou1234567890@outlook.com>
2024-04-03 14:56:57 +0300

    Update patch.js

--- patch.js
+++ patch.js
@@ -1,8 +1,8 @@
 let count = 100
-let inValidCount = -10
+let inValidCount = 0
 let validCount = 10
 
 function defaultCount(){
   document.getElementById("lev-icon").count = "30";
-  document.getElementById("lev-icon").type = "inValid";
+  document.getElementById("lev-icon").type = "valid";
 }
