16d0322cd08d80bed6bbcb00c7258abf992b8edb
Leva <levbulychou1234567890@outlook.com>
2026-06-11 10:38:03 +0300

    Update debug.js

--- js/debug.js
+++ js/debug.js
@@ -1,10 +1,6 @@
 let iconsVar = 510;
 let length = 10;
 let elementIndex = 10000;
-const binCode =
-bugChecker{
-    let maxBugs=0;
-};
 
 for (let index = 0; index < array.length; index++) {
     const element = array[index];
